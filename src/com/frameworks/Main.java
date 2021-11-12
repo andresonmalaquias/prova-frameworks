c7d1789d685ef86531b67331684a743ae0173b18
Gabriel Picanco <gabee.rodrigues@ifam.edu.br>
2021-11-12 16:34:15 -0400

    add Libs

--- src/com/frameworks/Main.java
+++ src/com/frameworks/Main.java
@@ -2,6 +2,10 @@ package com.frameworks;
 
 public class Main {
 
+    public static void CriarEstado(String[] args){
+
+    }
+
     public static void main(String[] args) {
 	// write your code here
     }
